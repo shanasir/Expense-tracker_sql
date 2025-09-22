a29292e79f42022b5f30a4a916595ac2f50ea7c3
Nasir <152576905+shanasir@users.noreply.github.com>
2025-09-23 00:20:40 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -4,7 +4,7 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST", "3306"),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", ""),
         database=os.getenv("DB_NAME", "expenses")
