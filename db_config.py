e0a1f39908924742c46c1287a32b6f2f9613dd85
Nasir <152576905+shanasir@users.noreply.github.com>
2025-09-23 19:14:31 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,11 +1,10 @@
-
-import os
 import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
-        password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "expenses")
+        host="caboose.proxy.rlwy.net",
+        port=21524,
+        user="root",
+        password="ETwRBCQNqljqYEnQmDXBHRJEcmyAKPcA",
+        database="expense"
     )
